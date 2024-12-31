f7c0702032b2261ce5dc02bb6727e5bcaf134b07
Prajwal Apchundekar <124507142+PrajwalApchundekar2023@users.noreply.github.com>
2024-12-31 12:41:05 +0000

    Container config done

---
+++ Basics/veriables.js
@@ -0,0 +1,37 @@
+/*  
+
+There is 3 types of veriable Config
+1- assignment 
+2- Declare
+3- Initilazation
+
+Variable Declaring
+        a) var
+        b) let
+        c) const
+
+const:
+- It is a block scope variable.
+- It allows only initialization.
+- It will not allow declaring and assignment.
+- It will not allow shadowing and hoisting.
+
+
+
+
+*/
+
+const accountNo=112555 // its valid
+let accountID=99;          // Cant assing the value again
+const name="Prajwal";
+let password="1234";
+
+console.log(accountID);
+console.table([accountNo,accountID,name,password])
+
+
+
+
+
+
+
